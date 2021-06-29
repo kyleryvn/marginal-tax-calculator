9086dec4af106ec17d95a944cc9c5084eb09c75c
Kyle <78947025+kyleryvn@users.noreply.github.com>
2021-06-29 18:39:26 -0400

    Create StartApplicationTests.java

---
+++ src/test/java/com/github/kyleryvn/marginaltax/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kyleryvn.marginaltax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+}
