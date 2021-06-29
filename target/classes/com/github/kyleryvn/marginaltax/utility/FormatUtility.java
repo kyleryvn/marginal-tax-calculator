e65d9b96332fc8fbfab94d31770ab4c62cbc84af
Kyle <78947025+kyleryvn@users.noreply.github.com>
2021-06-29 18:18:34 -0400

    Create FormatUtility.java

---
+++ target/classes/com/github/kyleryvn/marginaltax/utility/FormatUtility.java
@@ -0,0 +1,36 @@
+package com.github.kyleryvn.marginaltax.utility;
+
+import java.text.DecimalFormat;
+
+public class FormatUtility {
+
+    public static String customFormat(String pattern, double value) {
+        /*
+        Examples
+        customFormat("###,###.###", 123456.789);
+        customFormat("###.##", 123456.789);
+        customFormat("000000.000", 123.78);
+        customFormat("$###,###.###", 12345.67);
+        */
+
+        DecimalFormat myFormatter = new DecimalFormat(pattern);
+        return myFormatter.format(value);
+    }
+
+    public static double convertSalary(float number) {
+        return Double.parseDouble(Float.toString(number));
+    }
+
+    public static String convertFilingStatus(String fedFilingStatus) {
+        if (fedFilingStatus.equalsIgnoreCase("S"))
+            return "Single";
+        else if (fedFilingStatus.equalsIgnoreCase("MFJ"))
+            return "Married Filing Jointly";
+        else if (fedFilingStatus.equalsIgnoreCase("MFS"))
+            return "Married Filing Separately";
+        else if (fedFilingStatus.equalsIgnoreCase("HH"))
+            return "Head of Household";
+        else
+            return null;
+    }
+}
