99d00e9ba13199308acec4df46409f022a6c323f
Kyle <78947025+kyleryvn@users.noreply.github.com>
2021-06-29 18:17:59 -0400

    Create EnvironmentUtility.java

---
+++ src/main/java/com/github/kyleryvn/marginaltax/utility/EnvironmentUtility.java
@@ -0,0 +1,23 @@
+package com.github.kyleryvn.marginaltax.utility;
+
+import java.util.Map;
+import java.util.Set;
+
+public class EnvironmentUtility {
+    private static final Map<String, String> env = System.getenv();
+
+    public EnvironmentUtility() {
+    }
+
+    public static Set<String> keys() {
+        return env.keySet();
+    }
+
+    public static Map<String, String> getEnvironmentVariable() {
+        return env;
+    }
+
+    public static String get(String key) {
+        return (String) env.getOrDefault(key, "/notFound");
+    }
+}
